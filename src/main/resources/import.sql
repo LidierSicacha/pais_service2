b3bd7d382538c164f66a75a82fd7377a5d9d398a
Lidier Sicacha <lidier.sicacha01@uceva.edu.co>
2023-03-22 10:48:31 -0500

    Datos nuevos ingresado

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO pais (nombre) VALUES ('Colombia');
+INSERT INTO pais (nombre) VALUES ('Venezuela');
+INSERT INTO pais (nombre) VALUES ('Argentina');
+INSERT INTO pais (nombre) VALUES ('Peru');
+INSERT INTO pais (nombre) VALUES ('Ecuador');
+INSERT INTO pais (nombre) VALUES ('UK');
+INSERT INTO pais (nombre) VALUES ('US');
+INSERT INTO pais (nombre) VALUES ('Francia');
+INSERT INTO pais (nombre) VALUES ('Alemania');
+INSERT INTO pais (nombre) VALUES ('Italia');
